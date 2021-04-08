0c9813f5147e932f51cd190f65447f3bd245dbd7
Carlos Eduardo <carlos4hd@gmail.com>
2021-04-08 11:13:03 -0300

    teste2

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,12 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" teste ");
 
+		
+		int carlos = 1;
+		
+		System.out.println("carlos : " + carlos);
 	}
 
 }
